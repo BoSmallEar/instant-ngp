fc2310d203ab6766b39077378aa22f92843d9be6
Francesco Milano <francesco.milano172@gmail.com>
2022-04-13 11:51:51 +0200

    Minor: Style format in `scannet2nerf.py`

--- scripts/scannet2nerf.py
+++ scripts/scannet2nerf.py
@@ -4,35 +4,49 @@ import argparse
 import numpy as np
 import os
 
+
 def rotmat(a, b):
-	a, b = a / np.linalg.norm(a), b / np.linalg.norm(b)
-	v = np.cross(a, b)
-	c = np.dot(a, b)
-	s = np.linalg.norm(v)
-	kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
-	return np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2 + 1e-10))
+    a, b = a / np.linalg.norm(a), b / np.linalg.norm(b)
+    v = np.cross(a, b)
+    c = np.dot(a, b)
+    s = np.linalg.norm(v)
+    kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+    return np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s**2 + 1e-10))
 
-def closest_point_2_lines(oa, da, ob, db): # returns point closest to both rays of form o+t*d, and a weight factor that goes to 0 if the lines are parallel
-	da = da / np.linalg.norm(da)
-	db = db / np.linalg.norm(db)
-	c = np.cross(da, db)
-	denom = np.linalg.norm(c)**2
-	t = ob - oa
-	ta = np.linalg.det([t, db, c]) / (denom + 1e-10)
-	tb = np.linalg.det([t, da, c]) / (denom + 1e-10)
-	if ta > 0:
-		ta = 0
-	if tb > 0:
-		tb = 0
-	return (oa+ta*da+ob+tb*db) * 0.5, denom
 
-parser = argparse.ArgumentParser(description="Run neural graphics primitives testbed with additional configuration & output options")
+def closest_point_2_lines(
+    oa, da, ob, db
+):  # returns point closest to both rays of form o+t*d, and a weight factor that goes to 0 if the lines are parallel
+    da = da / np.linalg.norm(da)
+    db = db / np.linalg.norm(db)
+    c = np.cross(da, db)
+    denom = np.linalg.norm(c)**2
+    t = ob - oa
+    ta = np.linalg.det([t, db, c]) / (denom + 1e-10)
+    tb = np.linalg.det([t, da, c]) / (denom + 1e-10)
+    if ta > 0:
+        ta = 0
+    if tb > 0:
+        tb = 0
+    return (oa + ta * da + ob + tb * db) * 0.5, denom
+
+
+parser = argparse.ArgumentParser(
+    description=
+    "Run neural graphics primitives testbed with additional configuration & output options"
+)
 
 parser.add_argument("--scene_folder", type=str, default="")
-parser.add_argument("--transform_file", type=str, default="",)
-parser.add_argument("--interval", default=10, type=int, help="Sample Interval.")
+parser.add_argument(
+    "--transform_file",
+    type=str,
+    default="",
+)
+parser.add_argument("--interval",
+                    default=10,
+                    type=int,
+                    help="Sample Interval.")
 args = parser.parse_args()
-
 
 scannet_folder = args.scene_folder
 json_for_frame_selection = args.transform_file
@@ -45,46 +59,46 @@ with open(json_for_frame_selection, "r") as f:
     transforms = json.load(f)
 # - Get filenames and concurrently load the c2w.
 for frame_idx, frame in enumerate(transforms['frames']):
-	if (frame_idx % interval == 0):
-		frame_name = os.path.basename(frame['file_path']).split('.jpg')[0]
-		pose_name =	os.path.join(scannet_folder, f"pose/{frame_name}.txt")
-		c2w = np.loadtxt(pose_name)
-		print()
-		if np.any(np.isinf(c2w)):
-			continue
-		frame_names.append(frame_name)
-		c2ws.append(c2w)
+    if (frame_idx % interval == 0):
+        frame_name = os.path.basename(frame['file_path']).split('.jpg')[0]
+        pose_name = os.path.join(scannet_folder, f"pose/{frame_name}.txt")
+        c2w = np.loadtxt(pose_name)
+        print()
+        if np.any(np.isinf(c2w)):
+            continue
+        frame_names.append(frame_name)
+        c2ws.append(c2w)
 
 selected_transforms = copy.deepcopy(transforms)
 selected_transforms.pop('frames')
 selected_transforms['frames'] = []
 
-room_center = np.array([-0.0826264,1.37124763, 0.20387006])
-room_center_pos = np.array([4.234,1.8892, 1.4395])
+room_center = np.array([-0.0826264, 1.37124763, 0.20387006])
+room_center_pos = np.array([4.234, 1.8892, 1.4395])
 
 up = np.zeros(3)
 print(f"length of c2ws: {len(c2ws)}")
 for c2w_idx in range(len(c2ws)):
-	print(c2ws[c2w_idx])
-	c2ws[c2w_idx][:3,3] -= room_center_pos
-	print(c2ws[c2w_idx])
-	c2ws[c2w_idx][0:3,2] *= -1 # flip the y and z axis
-	c2ws[c2w_idx][0:3,1] *= -1
-	c2ws[c2w_idx] = c2ws[c2w_idx][[1,0,2,3],:] # swap y and z
-	c2ws[c2w_idx][2,:] *= -1 # flip whole world upside down
+    print(c2ws[c2w_idx])
+    c2ws[c2w_idx][:3, 3] -= room_center_pos
+    print(c2ws[c2w_idx])
+    c2ws[c2w_idx][0:3, 2] *= -1  # flip the y and z axis
+    c2ws[c2w_idx][0:3, 1] *= -1
+    c2ws[c2w_idx] = c2ws[c2w_idx][[1, 0, 2, 3], :]  # swap y and z
+    c2ws[c2w_idx][2, :] *= -1  # flip whole world upside down
 
-	up += c2ws[c2w_idx][0:3,1]
+    up += c2ws[c2w_idx][0:3, 1]
 print(f"up vector: {up}")
 
 nframes = len(c2ws)
 up = up / np.linalg.norm(up)
 print("up vector was", up)
-R = rotmat(up,[0,0,1]) # rotate up vector to [0,0,1]
-R = np.pad(R,[0,1])
+R = rotmat(up, [0, 0, 1])  # rotate up vector to [0,0,1]
+R = np.pad(R, [0, 1])
 R[-1, -1] = 1
 
 for c2w_idx in range(len(c2ws)):
-	c2ws[c2w_idx] = np.matmul(R, c2ws[c2w_idx]) # rotate up to be the z axis
+    c2ws[c2w_idx] = np.matmul(R, c2ws[c2w_idx])  # rotate up to be the z axis
 
 # find a central point they are all looking at
 # print("computing center of attention...")
@@ -105,23 +119,25 @@ for c2w_idx in range(len(c2ws)):
 
 avglen = 0.
 for c2w_idx in range(len(c2ws)):
-	avglen += np.linalg.norm(c2ws[c2w_idx][0:3,3])
+    avglen += np.linalg.norm(c2ws[c2w_idx][0:3, 3])
 avglen /= nframes
 print("avg camera distance from origin", avglen)
 for c2w_idx in range(len(c2ws)):
-	c2ws[c2w_idx][0:3,3] *= 4.0 / avglen # scale to "nerf sized"
+    c2ws[c2w_idx][0:3, 3] *= 4.0 / avglen  # scale to "nerf sized"
 
 curr_frame_name_idx = 0
 for frame_idx in range(len(transforms['frames'])):
-	if (curr_frame_name_idx == len(frame_names)):
-		break
-	frame = transforms['frames'][frame_idx]
-	frame_name = os.path.basename(frame['file_path']).split('.jpg')[0]
+    if (curr_frame_name_idx == len(frame_names)):
+        break
+    frame = transforms['frames'][frame_idx]
+    frame_name = os.path.basename(frame['file_path']).split('.jpg')[0]
+    if (frame_name == frame_names[curr_frame_name_idx]):
 	if (frame_name == frame_names[curr_frame_name_idx]):	
-		c2w = c2ws[curr_frame_name_idx]
-		transforms['frames'][frame_idx]['transform_matrix'] = c2w.tolist()
-		selected_transforms['frames'].append(transforms['frames'][frame_idx])
-		curr_frame_name_idx += 1
+    if (frame_name == frame_names[curr_frame_name_idx]):
+        c2w = c2ws[curr_frame_name_idx]
+        transforms['frames'][frame_idx]['transform_matrix'] = c2w.tolist()
+        selected_transforms['frames'].append(transforms['frames'][frame_idx])
+        curr_frame_name_idx += 1
 
 with open(f"transforms_train_{interval}_modified_center.json", "w") as f:
     json.dump(obj=selected_transforms, fp=f)
